9cb507c2102eee0268d7afc1cd67bab0a9f0cb98
Georgi <gogolino1982@gmail.com>
2023-11-12 14:26:56 +0200

    Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Honey from "./components/Honey";
 import Header from "./components/Header";
+import Spinner from "./components/Spinner";
 
 function App() {
   return (
     <div>
-      {/* <div className="loader_bg">
-				<div className="loader">
-					<img src="images/loading.gif" alt="#" />
-				</div>
-			</div> */}
+      <Spinner/>
       <Header />
       {/* Honey for homePage */}
       <Honey />
